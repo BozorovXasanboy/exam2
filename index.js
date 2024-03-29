23061df6bb9659474cb37db5c8f07adf47ff7270
BozorovXasanboy <xbozorov091@gmail.com>
2024-03-29 15:40:30 +0300

    exam2

---
+++ index.js
@@ -0,0 +1,62 @@
+// 1.// let a = 2;
+// // let b = 5;
+
+// // if(a > b){
+// //     console.log("a katta son");
+// // } else{
+// //     console.log("b katta son");
+// // }
+
+2.
+3.
+// 4.  A)   <script src="./script.js" />
+
+// 5.<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Btn Exampl</title>
+//     <style>
+//         body {
+//             transition: background-color 0.5s ease;
+//         }
+//         .qora-background {
+//             background-color: black;
+//             color: white;
+//         }
+//     </style>
+// </head>
+// <body>
+
+// <button class="btn">Button</button>
+
+// <script>
+//     document.querySelector('.btn').addEventListener('click', function() {
+//         document.body.classList.toggle('qora-background');
+//     });
+// </script>
+
+// </body>
+// </html>
+// // 6.<!DOCTYPE html>
+// // <html lang="en">
+// // <head>
+// //     <meta charset="UTF-8">
+// //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// //     <title>Toq Som</title>
+// // </head>
+// // <body>
+
+// // <script>
+// //     for (let i = 1; i <= 20; i += 2) {
+//         document.write(i + "<br>");
+//     }
+// </script>
+
+// </body>
+// </html>
+/* // 7.A)    const btn = document.querySelector(".btn");
+// 8.B)   btn.style.backgroundColor = “red”;
+// 9.	D)   h1, div, p;
+// 10.D)  <link href="./style.css" /> */
